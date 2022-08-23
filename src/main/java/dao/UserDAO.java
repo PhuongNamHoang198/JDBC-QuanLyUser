856e63ba6e95cd4196a719d43abe02ee7b5aa9b0
PhuongNamHoang198 <hoangphuongnam.98@gmail.com>
2022-08-23 18:06:24 +0700

    update UserDAO

--- src/main/java/dao/UserDAO.java
+++ src/main/java/dao/UserDAO.java
@@ -3,6 +3,7 @@ package dao;
 import model.User;
 
 import java.sql.*;
+import java.util.ArrayList;
 import java.util.List;
 
 public class UserDAO implements IUserDAO{
@@ -34,19 +35,7 @@ private static final String INSERT_USER_SQL ="INSERT INTO users (name,email,coun
         return connection;
     }
 
-//    public void insertUser(User user) throws SQLException {
-//        System.out.println(INSERT_USERS_SQL);
-//        // try-with-resource statement will auto close the connection.
-//        try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USERS_SQL)) {
-//            preparedStatement.setString(1, user.getName());
-//            preparedStatement.setString(2, user.getEmail());
-//            preparedStatement.setString(3, user.getCountry());
-//            System.out.println(preparedStatement);
-//            preparedStatement.executeUpdate();
-//        } catch (SQLException e) {
-//            printSQLException(e);
-//        }
-//    }
+
     public void insertUser(User user) throws SQLException {
         System.out.println(INSERT_USER_SQL);
         try (Connection connection = getConnection(); PreparedStatement preparedStatement = connection.prepareStatement(INSERT_USER_SQL)) {
@@ -58,64 +47,93 @@ private static final String INSERT_USER_SQL ="INSERT INTO users (name,email,coun
         }
     }
 
-//    public User selectUser(int id) {
-//        User user = null;
-//        // Step 1: Establishing a Connection
-//        try (Connection connection = getConnection();
-//             // Step 2:Create a statement using connection object
-//             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID);) {
-//            preparedStatement.setInt(1, id);
-//            System.out.println(preparedStatement);
-//            // Step 3: Execute the query or update query
-//            ResultSet rs = preparedStatement.executeQuery();
-//
-//            // Step 4: Process the ResultSet object.
-//            while (rs.next()) {
-//                String name = rs.getString("name");
-//                String email = rs.getString("email");
-//                String country = rs.getString("country");
-//                user = new User(id, name, email, country);
-//            }
-//        } catch (SQLException e) {
-//            printSQLException(e);
-//        }
-//        return user;
-//    }
 
 
-    @Override
     public User selectUser(int id) {
         User user = null;
+        // Step 1: Establishing a Connection
         try (Connection connection = getConnection();
-        PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID);) {
-            PreparedStatement.setInt(1, id);
+             // Step 2:Create a statement using connection object
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_USER_BY_ID);) {
+            preparedStatement.setInt(1, id);
             System.out.println(preparedStatement);
+            // Step 3: Execute the query or update query
             ResultSet rs = preparedStatement.executeQuery();
 
+            // Step 4: Process the ResultSet object.
             while (rs.next()) {
                 String name = rs.getString("name");
                 String email = rs.getString("email");
                 String country = rs.getString("country");
+                user = new User(id, name, email, country);
             }
-
-        }catch (SQLException e) {
-
+        } catch (SQLException e) {
+            printSQLException(e);
         }
         return user;
     }
 
     @Override
     public List<User> selectAllUsers() {
-        return null;
+        List<User> users = new ArrayList<>();
+        // Step 1 thiết lập kết nối
+        try (Connection connection = getConnection();
+
+             // Step 2:tao cau lenh su dung doi tuong ket noi
+             PreparedStatement preparedStatement = connection.prepareStatement(SELECT_ALL_USERS);) {
+            System.out.println(preparedStatement);
+            // Step 3: thực thi truy vấn hoặc cập nhật truy vấn
+            ResultSet rs = preparedStatement.executeQuery();
+
+            // Step 4: xử lý đối tượng ResultSet
+            while (rs.next()) {
+                int id = rs.getInt("id");
+                String name = rs.getString("name");
+                String email = rs.getString("email");
+                String country = rs.getString("country");
+                users.add(new User(id, name, email, country));
+            }
+        } catch (SQLException e) {
+            printSQLException(e);
+        }
+        return users;
     }
 
-    @Override
     public boolean deleteUser(int id) throws SQLException {
-        return false;
+        boolean rowDeleted;
+        try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement(DELETE_USERS_SQL);) {
+            statement.setInt(1, id);
+            rowDeleted = statement.executeUpdate() > 0;
+        }
+        return rowDeleted;
     }
 
-    @Override
     public boolean updateUser(User user) throws SQLException {
-        return false;
+        boolean rowUpdated;
+        try (Connection connection = getConnection(); PreparedStatement statement = connection.prepareStatement(UPDATE_USERS_SQL);) {
+            statement.setString(1, user.getName());
+            statement.setString(2, user.getEmail());
+            statement.setString(3, user.getCountry());
+            statement.setInt(4, user.getId());
+
+            rowUpdated = statement.executeUpdate() > 0;
+        }
+        return rowUpdated;
+    }
+
+    private void printSQLException(SQLException ex) {
+        for (Throwable e : ex) {
+            if (e instanceof SQLException) {
+                e.printStackTrace(System.err);
+                System.err.println("SQLState: " + ((SQLException) e).getSQLState());
+                System.err.println("Error Code: " + ((SQLException) e).getErrorCode());
+                System.err.println("Message: " + e.getMessage());
+                Throwable t = ex.getCause();
+                while (t != null) {
+                    System.out.println("Cause: " + t);
+                    t = t.getCause();
+                }
+            }
+        }
     }
 }
